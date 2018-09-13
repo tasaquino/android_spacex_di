e18450571add874cfd743654cdc0cad33cf10be7
tasaquino <thaisandrade@live.com>
2018-09-12 22:58:49 -0300

    some cleaning

--- app/src/main/java/tasaquino/com/spacexdi/di/components/AppComponent.kt
+++ app/src/main/java/tasaquino/com/spacexdi/di/components/AppComponent.kt
@@ -1,24 +1,16 @@
 package tasaquino.com.spacexdi.di.components
 
-import android.app.Application
 import dagger.Component
-import dagger.android.AndroidInjectionModule
 import dagger.android.support.AndroidSupportInjectionModule
 import tasaquino.com.spacexdi.SpacexApplication
 import tasaquino.com.spacexdi.di.modules.ActivitiesBuilderModule
-import tasaquino.com.spacexdi.di.modules.AppModule
-import tasaquino.com.spacexdi.di.modules.NetworkingModule
-import tasaquino.com.spacexdi.networking.SpacexAPI
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [
-    NetworkingModule::class,
     ActivitiesBuilderModule::class,
-    AndroidSupportInjectionModule::class,
-    AppModule::class
+    AndroidSupportInjectionModule::class
 ])
 interface AppComponent {
     fun inject(app: SpacexApplication)
-    fun api(): SpacexAPI
 }
