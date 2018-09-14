3edbe5150f82d3731866a6df3084b824eecc52f3
tasaquino <thaisandrade@live.com>
2018-09-14 20:59:54 -0300

    fix import

--- app/src/main/java/tasaquino/com/spacexdi/SpacexApplication.kt
+++ app/src/main/java/tasaquino/com/spacexdi/SpacexApplication.kt
@@ -3,7 +3,6 @@ package tasaquino.com.spacexdi
 import android.app.Application
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
-import tasaquino.com.spacexdi.di.modules.appModule
 import tasaquino.com.spacexdi.di.modules.networkingModule
 import tasaquino.com.spacexdi.di.modules.rocketsPresenterModule
 import tasaquino.com.spacexdi.di.modules.rocketsSourceModule
