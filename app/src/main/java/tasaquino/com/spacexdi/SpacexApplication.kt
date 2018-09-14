b81b6e446fcf1bacfff6012344a06d7f4b7083ba
tasaquino <thaisandrade@live.com>
2018-09-14 20:58:54 -0300

    importing modules in application

--- app/src/main/java/tasaquino/com/spacexdi/SpacexApplication.kt
+++ app/src/main/java/tasaquino/com/spacexdi/SpacexApplication.kt
@@ -4,9 +4,16 @@ import android.app.Application
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import tasaquino.com.spacexdi.di.modules.appModule
+import tasaquino.com.spacexdi.di.modules.networkingModule
+import tasaquino.com.spacexdi.di.modules.rocketsPresenterModule
+import tasaquino.com.spacexdi.di.modules.rocketsSourceModule
 
 class SpacexApplication : Application(), KodeinAware {
     override val kodein: Kodein = Kodein.lazy {
-        import(appModule)
+        import(networkingModule)
+        import(rocketsSourceModule)
+        import(rocketsPresenterModule)
     }
 }
+
+
--- app/src/main/java/tasaquino/com/spacexdi/di/modules/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package tasaquino.com.spacexdi.di.modules
-
-import org.kodein.di.Kodein
-
-val appModule = Kodein.Module("appDiModule") {
-    import(networkingModule)
-    import(rocketsSourceModule)
-    import(rocketsPresenterModule)
-}
